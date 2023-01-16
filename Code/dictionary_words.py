1afb2f6b3fa01831a3533fde8abd922d11b85eea
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-01-15 16:52:32 -0800

    fixed rearrange.py

---
+++ Code/dictionary_words.py
@@ -0,0 +1,4 @@
+import random
+
+def random_dictionary_words():
+    
--- Code/rearrange.py
+++ Code/rearrange.py
@@ -1,11 +1,12 @@
 import random
+import sys
 
 def rearrange_words(set_of_words):
     random.shuffle(set_of_words)
     output = " ".join(set_of_words)
-
     print(output)
 
+
 if __name__ == '__main__':
-    set_of_words = input(str("Enter A List Of Words: ")).split()
+    set_of_words = sys.argv[1:]
     rearrange_words(set_of_words)
