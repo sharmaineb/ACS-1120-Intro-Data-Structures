26448d330e403554513c8fa773c23a6acd8775d7
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-01-17 11:39:06 -0800

    added dictionary_words.py

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -1,4 +1,13 @@
 import random
+import sys
 
-def random_dictionary_words():
+def random_dictionary_words(word_amount, dictionary_file):
+    with open(dictionary_file) as file:
+        random_words = file.read().splitlines()
+        user_input_amount = random.choices(random_words, k=int(word_amount))
+        output = " ".join(user_input_amount)
+        return print(output)
+
+if __name__ == "__main__":
+    random_dictionary_words(int(sys.argv[1]), '/usr/share/dict/words')
     
