52d6e37966217373fd1cdbd69f2d73d92c375f7a
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-01-17 23:20:32 -0800

    refactored quote_user.py

--- Code/quote_user.py
+++ Code/quote_user.py
@@ -1,5 +1,4 @@
-from python_quote import random_python_quote
+import python_quote
 
 if __name__ == '__main__':
-    quote = random_python_quote()
-    print(quote)
+    print(python_quote.random_python_quote)
