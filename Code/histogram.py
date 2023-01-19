34c0be769857ed770811d3f3b14f7e314602072f
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-01-18 17:17:09 -0800

    [add] histogram.py file

---
+++ Code/histogram.py
@@ -0,0 +1,18 @@
+# histogram() function that takes a source_text argument
+def histogram(source_text):
+
+
+
+
+
+
+# unique_words() function that takes a histogram argument
+def unique_words(histogram):
+
+
+
+
+# frequency() function that takes a word and histogram argument
+# returns the total count of unique words in the histogram
+def frequency(word, histogram):
+
