ac74ee319a10abb6310b1bc94c6a0349a542ee1f
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-01-20 17:14:27 -0800

    [update] working on histogram.py

--- Code/histogram.py
+++ Code/histogram.py
@@ -1,5 +1,8 @@
 # histogram() function that takes a source_text argument
 def histogram(source_text):
+    histogram_dict = {}
+    with open(source_text) as book_source: 
+        book_text = book_source.read()
 
 
 
@@ -12,7 +15,18 @@ def unique_words(histogram):
 
 
 
-# frequency() function that takes a word and histogram argument
-# returns the total count of unique words in the histogram
+# # frequency() function that takes a word and histogram argument
+# # returns the total count of unique words in the histogram
 def frequency(word, histogram):
 
+
+
+
+
+
+
+
+
+
+# if __name__ == "__main__":
+    
