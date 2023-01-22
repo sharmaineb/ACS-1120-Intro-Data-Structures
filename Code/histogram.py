38816a482b283d044c3e608b7a2ce8c2a96a517e
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-01-22 02:26:21 -0800

    [update] progress

--- Code/histogram.py
+++ Code/histogram.py
@@ -2,31 +2,38 @@
 def histogram(source_text):
     histogram_dict = {}
     with open(source_text) as book_source: 
-        book_text = book_source.read()
-
-
-
-
+        book_content = book_source.read()
+        book_text = book_content.split()
+        for word in book_text:
+            if word in histogram_dict:
+                histogram_dict[word] += 1
+            else:
+                histogram_dict[word] = 1
+        print(histogram_dict)
+        return histogram_dict
 
 
 # unique_words() function that takes a histogram argument
 def unique_words(histogram):
+    unique_word_count = 0
+    for each in histogram.word_count():
+        unique_word_count += 1
 
-
+    print(unique_word_count)
+    return unique_word_count
 
 
 # # frequency() function that takes a word and histogram argument
 # # returns the total count of unique words in the histogram
 def frequency(word, histogram):
+    for word in histogram.word_count():
+        if word == word:
+            word_frequency = histogram[word]
+
+    print(word_frequency)
+    return word_frequency
 
 
-
-
-
-
-
-
-
-
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    histogram("book.txt")
     
