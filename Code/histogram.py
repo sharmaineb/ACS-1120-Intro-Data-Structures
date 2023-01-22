bfe7f3f5fa7e289ae46db1e64c16daa2bd56a4a3
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-01-22 14:24:32 -0800

    [update] working on tuple

--- Code/histogram.py
+++ Code/histogram.py
@@ -1,6 +1,8 @@
 # histogram() function that takes a source_text argument
 def histogram(source_text):
-    histogram_dict = {}
+    # start with an empty array that the words will be added to
+    histogram_dict = {} 
+    # splits text file into words and adds to histogram_dict array/ returns list of strings
     with open(source_text) as book_source: 
         book_content = book_source.read()
         book_text = book_content.split()
@@ -33,7 +35,20 @@ def frequency(word, histogram):
     print(word_frequency)
     return word_frequency
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~ TUPLE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def histogram_book_tuple(source_text):
+    # start with an empty dictionary
+    histogram_book_tuple = []
+    # splits text file into words/ returns list of strings
+    with open(source_text) as book_content:
+        book_text = book_content.read().split()
+
+
+
+
+
 
 if __name__ == "__main__":
     histogram("book.txt")
+    #histogram_book_tuple("sample.txt")
     
