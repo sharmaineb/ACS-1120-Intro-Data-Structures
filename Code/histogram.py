d8ab8f77f4da77ac093f9cac4faab6c22585f789
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-01-22 22:55:53 -0800

    [add] small updates

--- Code/histogram.py
+++ Code/histogram.py
@@ -9,8 +9,8 @@ def histogram(source_text):
                 histogram_dict[word] += 1
             else:
                 histogram_dict[word] = 1
-        print(histogram_dict)
-        return histogram_dict
+        return print(histogram_dict)
+        
 
 
 # unique_words() function that takes a histogram argument
@@ -19,8 +19,8 @@ def unique_words(histogram):
     for each in histogram.word_count():
         unique_word_count += 1
 
-    print(unique_word_count)
-    return unique_word_count
+    return print(unique_word_count)
+    
 
 
 # # frequency() function that takes a word and histogram argument
@@ -30,18 +30,17 @@ def frequency(word, histogram):
         if word == word:
             word_frequency = histogram[word]
 
-    print(word_frequency)
-    return word_frequency
+    return print(word_frequency) 
 
 # tuple
-# def histogram_book_tuple(source_text):
-#     histogram_book_tuple = []
-#     with open(source_text) as book_content:
-#         book_text = book_content.read().split()
-#     for word in range(len(book_text)):
-#         histogram_book_tuple.append((book_text[word], book_text.count(book_text[word])))
-#     print(histogram_book_tuple)
-#     return histogram_book_tuple
+def histogram_book_tuple(source_text):
+    histogram_book_tuple = []
+    with open(source_text) as book_content:
+        book_text = book_content.read().split()
+    for word in range(len(book_text)):
+        histogram_book_tuple.append((book_text[word], book_text.count(book_text[word])))
+    print(histogram_book_tuple)
+    return histogram_book_tuple
 
 if __name__ == "__main__":
     histogram("book.txt")
