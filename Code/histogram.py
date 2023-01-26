d486d2acf74c9c26e970e67fbec941103d93fcbc
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-01-25 21:51:21 -0800

    {update] clean-up & functional

--- Code/histogram.py
+++ Code/histogram.py
@@ -1,60 +1,77 @@
 # histogram() function that takes a source_text argument
-def histogram(source_text):
-    histogram_dict = {}
-    with open(source_text) as book_source: 
-        book_content = book_source.read()
-        book_text = book_content.split()
-        for word in book_text:
+histogram_dict = {}
+
+def generate_histogram(filename):
+    with open(filename) as file:
+        words = file.read().split()
+        for word in words:
             if word in histogram_dict:
                 histogram_dict[word] += 1
             else:
                 histogram_dict[word] = 1
+        for key in list(histogram_dict.keys()):
+            print(key, ":", histogram_dict[key])
         return print(histogram_dict)
 
-# def histogram(filename):
-#     histogram_dict = {}
-#     with open(filename) as book_source: 
-#         book_content = book_source.read()
-#         book_text = book_content.split()
-#         for word in book_text:
-#             word_count = histogram_dict.get(word, 0) + 1
-#             histogram_dict[word] = word_count
-#         return print(histogram_dict)
         
 # unique_words() function that takes a histogram argument
-def unique_words(histogram):
-    # unique_word_count = 0
-    # for each in histogram.word_count():
-    #     unique_word_count += 1
-    return len(histogram.items())
-
-    
+def unique_words(histogram_dict):
+    unique_word_count = len(histogram_dict)
+    return unique_word_count
     
 # frequency() function that takes a word and histogram argument
 # returns the total count of unique words in the histogram
-def frequency(word, histogram):
-    # for word in histogram.word_count():
-    #     if word == word:
-    #         word_frequency = histogram[word]
-    if word not in histogram:
-        return("Word Not Found in Histogram.")
+def frequency(word, histogram_dict):
+    frequent_words = histogram_dict[word]
+    return frequent_words
 
-    return print(histogram[word])
-
-
-# # tuple
-# def histogram_book_tuple(filename):
-#     histogram_book_tuple = []
-#     with open(filename) as book_content:
-#         book_text = book_content.read().split()
-#     for word in range(len(book_text)):
-#         histogram_book_tuple.append((book_text[word], book_text.count(book_text[word])))
-#     return print(histogram_book_tuple)
+# tuple
+def histogram_book_tuple(filename):
+    histogram_book_tuple = []
+    with open(filename) as book_content:
+        book_text = book_content.read().split()
+    for word in range(len(book_text)):
+        histogram_book_tuple.append((book_text[word], book_text.count(book_text[word])))
+    return print(histogram_book_tuple)
     
 
 if __name__ == "__main__":
-    histogram("fish_example.txt")
-    # histogram("book.txt")
-    # histogram_book_tuple("book.txt")
+    generate_histogram('fish_example.txt')
+    unique_words(histogram_dict)
+    # generate_histogram("book.txt")
+    # generate_histogram_book_tuple("book.txt")
 
-    
+# attempts:
+
+# def histogram(source_text):
+#     histogram_dict = {}
+#     with open(source_text) as book_source: 
+#         book_content = book_source.read()
+#         book_text = book_content.split()
+#         for word in book_text:
+#             if word in histogram_dict:
+#                 histogram_dict[word] += 1
+#             else:
+#                 histogram_dict[word] = 1
+#         return print(histogram_dict)
+
+# for key in histogram:
+#     if histogram[key] == 1:
+#         unique_word_count += 1
+# return len(unique_word_count)
+# unique_word_count = 0
+# for key in histogram:
+#     if histogram[key] == 1:
+#         unique_word_count += 1
+# return unique_word_count
+
+# if word in histogram.keys():
+#     return histogram[word]
+# return 0
+# for word in histogram.word_count():
+#     if word == word:
+#         word_frequency = histogram[word]
+# if word not in histogram:
+#     return("Word Not Found in Histogram.")
+
+# return print(histogram[word])
--- Code/sample.py
+++ Code/sample.py
@@ -1,72 +1,32 @@
 import random
-from histogram import histogram
+from histogram import *
 import sys
 
 # stochastic sampling means taking an element from a given collection at random.
 
 # write a function that takes a histogram and returns a single word, at random
 
-def single_word(histogram):
-    output = []
-    random_word = random.randint(0, len(histogram)- 1)
-    for word in histogram:
-        output.append[word]
-    return output[random_word]
-
-# def single_word(histogram):
-#     all_words = histogram(histogram)
-#     random_word = random.choices(
-#         list(all_words.items()),
-#         weight_words = all_words.values(),
-#         k = 1) [0]
-#     return random_word
-
-# def single_word(histogram):
-#     output = []
-#     random_word = random.randint(0, len(histogram) -1)
-#     for word in histogram:
-#         output.append[word]
-#     return output[random_word]
+def single_word(histogram_dict):
+    output = list(histogram_dict.keys())
+    output_index = random.randint(0, len(output) - 1)
+    random_word = output[output_index]
+    return print(random_word)
 
 def word_count(histogram):
-    random_word = random.randint(1, sum(histogram.items()))
-    for word, item in histogram.items():
-        random_word -= item
-        if random_word > 0:
-            continue
-        else:
-            return word
+    count = 0
+    output = random.randint(0, sum(histogram.values()))
+    for key, value in histogram.items():
+        count += value
+        if count >= output:
+            return key
 
-
-def prob_word(histogram):
-    loopy = {}
-    for word in range(10000):
-        word = single_word(histogram)
-        if word in loopy:
-            loopy[word] += 1
-        else:
-            loopy[word] = 1
-    return loopy
 
 if __name__ == "__main__":
-    # source_text = "one fish two fish three fish four fish"
-    # source_text = histogram("fish_example.txt")
-    # # sample = single_word(source_text)
-    # # print(sample)
+    filename = sys.argv[1]
+    histo = generate_histogram(filename)
+    print(histo)
+    print(word_count(histogram_dict))
     
-    # # prob = prob_word(source_text)
-    # # for word in prob:
-    # #     print(f"{word}: {prob [word]}")
-    # word = histogram(histogram)
-    # print(word)
-    # word_output = single_word(word)
-
-    # count = prob_word(word)
-    # prob_word(word)
-    # print(count)
-
-# thank you Andrew A. for the guidance!
-
 # a word with a higher weighting will have a higher probability of being selected
 # frequency weighting: words which appear more frequently in the original text should be more likely
 # to be selected by our sampling program.
