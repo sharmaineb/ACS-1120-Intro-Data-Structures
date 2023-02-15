29a404f7f85116e6c2cec81e8555703baf8c59a0
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-02-14 21:40:29 -0800

    [update] failed 1 linkedlist.py test

--- Code/linkedlist.py
+++ Code/linkedlist.py
@@ -86,13 +86,13 @@ class LinkedList:
             node.next = self.head
         self.head = node
 
-    def find(self, matcher):
+    def find(self, item):
         """Return an item from this linked list if it is present.
         TODO: Best case running time: O(???) Why and under what conditions?
         TODO: Worst case running time: O(???) Why and under what conditions?"""
         # TODO: Loop through all nodes to find item, if present return True otherwise False
-        for node in self.matcher():
-            if matcher == node:
+        for node in self.items():
+            if item == node:
                 return True
         return False
 
