bbf74b2ba72a7b60730fc658b7b38e1f20433eae
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-02-17 12:37:45 -0800

    [update] working on linkedlist.py

--- Code/markov.py
+++ Code/markov.py
@@ -1,63 +1,7 @@
 import random
 
 class Markov():
-    def __init__(self, word_list):
-        self.word_list = self.read_source(word_list)
-    
-    def read_source(self, word_list):
-        with open (word_list) as source_text:
-            source_text = source_text.read().split()
-        return source_text
-    
-    def onset(self):
-        source_text = self.word_list
-        beginning_word = []
-        for word in source_text:
-            if word[0].isupper() == True:
-                beginning_word.append(word)
-        return random.choice(beginning_word)
-    
-    def end(self):
-        source_text = self.word_list
-        end_word = []
-        for word in source_text:
-            if word[-1] in [".", "!", "?", ";"]:
-                end_word.append(word)
-        return random.choice(end_word)
 
-    def markov_walk(self):
-        text = self.word_list
-        markov_dict = {}
-        for index in range(len(text) - 1):
-            following_word = text[index + 1]
-            if text[index] in markov_dict:
-                markov_dict[text[index]].append(following_word)
-            else:
-                markov_dict[text[index]] = [following_word]
-        return markov_dict
-    
-    def sentence(self, amount):
-        walk = self.markov_walk() 
-        onset = self.onset()
-        end = self.end()
-
-        sentence = [onset]
-        following_word = onset
-        amount -= 2
-
-        for i, _ in enumerate(walk, start=1):
-            try:
-                following_word = random.choice(walk[following_word])
-            except:
-                return " ".join(sentence) + f" {end}"
-            sentence.append(following_word)
-            if following_word == end or i == amount:
-                return " ".join(sentence) + f" {end}"
-
-if __name__ == "__main__":
-    text = "./data/b99.txt"
-    markov = Markov(text)
-    print(markov.sentence(10))
 
         
 
