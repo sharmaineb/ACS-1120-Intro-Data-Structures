8de2a0a0fde6b83148d90fd5389e2002090dab12
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-02-23 12:58:04 -0800

    [update] app.py

--- Code/app.py
+++ Code/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     """Route that returns a web page containing the generated text."""
-    word_source = read_source("b99.txt")
+    word_source = read_source("./data/b99.txt")
     output_sentence = output_random(word_source, 80)
     return output_sentence
 
