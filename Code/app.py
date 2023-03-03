c3cf41a1b94a317d0569d3615dc67e71fcf7cb5f
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-03-02 16:02:30 -0800

    [update] cleaning up

--- Code/app.py
+++ Code/app.py
@@ -22,7 +22,4 @@ def home():
 
 
 if __name__ == "__main__":
-    """To run the Flask server, execute `python app.py` in your terminal.
-       To learn more about Flask's DEBUG mode, visit
-       https://flask.palletsprojects.com/en/2.0.x/server/#in-code"""
     app.run(debug=True)
